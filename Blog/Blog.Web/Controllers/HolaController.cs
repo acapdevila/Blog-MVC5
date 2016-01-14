627c4eaf1e4cad6ca036636bb63ce4bb4d6c37a7
Albert Capdevila <albert@acprogramador.com>
2016-01-14 21:59:21 +0100

    Página de presentación "Hola mi nombre es..."

--- Blog/Blog.Web/Controllers/HolaController.cs
+++ Blog/Blog.Web/Controllers/HolaController.cs
@@ -6,6 +6,11 @@ namespace Blog.Web.Controllers
     {
         public ActionResult Index()
         {
+            return View("MiNombreEsAlbert");
+        }
+
+        public ActionResult MiNombreEsAlbert()
+        {
             return View();
         }
     }
