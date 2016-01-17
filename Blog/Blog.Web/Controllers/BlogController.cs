e5002f1af538b2ef9f93944f1692a650f5bce0a7
Albert Capdevila <albert@acprogramador.com>
2016-01-17 18:14:05 +0100

    Sidebar con nube de etiquetas

--- Blog/Blog.Web/Controllers/BlogController.cs
+++ Blog/Blog.Web/Controllers/BlogController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class BlogController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Etiqueta(string id)
         {
             return View();
         }
---
+++ Blog/Blog.Web/Controllers/SidebarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Blog.Datos;
+using Blog.Modelo;
+using Blog.Web.ViewModels.Sidebar;
+
+
+namespace Blog.Web.Controllers
+{
+    public class SidebarController : Controller
+    {
+        private readonly ContextoBaseDatos db = new ContextoBaseDatos();
+
+        [ChildActionOnly]
+        public ActionResult NubeEtiquetas()
+        {
+            var etiquetas = db.Tags.ToList();
+            var nubeEtiquetasViewModel = new NubeEtiquetasViewModel(etiquetas);
+
+            return View(nubeEtiquetasViewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+   
+}
