25f0b8e1324fd4641797e6a471b9904e282ee67f
Albert Capdevila <albert@acprogramador.com>
2016-01-25 22:54:35 +0100

    Definición de ruta para un post dia/mes/anyo/slug

--- Blog/Blog.Web/App_Start/RouteConfig.cs
+++ Blog/Blog.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace Blog.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "BlogPost",
+              url: "{dia}/{mes}/{anyo}/{urlSlug}",
+              defaults: new { controller = "Blog", action = "Detalles" }//,
+              //constraints: new { dia = @"d{4}-d{2}-d{2}" }
+          );
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
--- Blog/Blog.Web/Controllers/BlogController.cs
+++ Blog/Blog.Web/Controllers/BlogController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Blog.Datos;
+using Blog.Modelo;
 using Blog.Web.ViewModels.Blog;
 
 namespace Blog.Web.Controllers
@@ -36,9 +38,49 @@ namespace Blog.Web.Controllers
             };
         }
 
+        public async Task<ActionResult> Detalles(string dia, string mes, string anyo, string urlSlug)
+        {
+            if (string.IsNullOrEmpty(urlSlug))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var fechaPost = GenerarFecha(dia, mes, anyo);
+
+            if(fechaPost == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var post = await RecuperarPost(fechaPost.Value, urlSlug);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        private DateTime? GenerarFecha(string dia, string mes, string anyo)
+        {
+            int numDia;
+            if (!int.TryParse(dia, out numDia)) return null;
+
+            int numMes;
+            if (!int.TryParse(mes, out numMes)) return null;
+
+            int numAnyo;
+            if (!int.TryParse(anyo, out numAnyo)) return null;
+
+            return new DateTime(numAnyo, numMes, numDia);
+
+        }
+
         public ActionResult Etiqueta(string id)
         {
             return View();
+        }
+
+        private async Task<Post> RecuperarPost(DateTime fechaPost, string urlSlug)
+        {
+            return await _db.Posts.Include(m => m.Tags)
+                            .FirstOrDefaultAsync(m => m.UrlSlug == urlSlug && m.FechaPost == fechaPost);
         }
     }
 }
