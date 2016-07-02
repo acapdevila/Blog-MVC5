da298a7c01aa5daa8fbd9f0fa22bc6b7d1ea11f6
Albert <albert.capdevila@buclenet.com>
2016-07-02 13:06:31 +0200

    Inicio segundo blog: Smoothies de cuchara

---
+++ Blog/Blog.Smoothies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog.Smoothies.Startup))]
+namespace Blog.Smoothies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
