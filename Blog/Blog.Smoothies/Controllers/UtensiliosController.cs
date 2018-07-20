78b918ad928328583eb5a61f35129cc14b0516ff
Laura <laura.garcia@buclenet.com>
2018-07-20 11:27:11 +0200

    Nou apartat Libros

---
+++ Blog/Blog.Smoothies/Controllers/UtensiliosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Smoothies.Controllers
+{
+    public class UtensiliosController : Controller
+    {
+        // GET: Utensilios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
