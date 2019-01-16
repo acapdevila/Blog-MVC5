d2d0329bd23245b250e5d91d7970c74922f0c6a4
Albert <albert.capdevila@buclenet.com>
2019-01-16 10:15:04 +0100

    BlogViewModel en ByLaura

---
+++ Blog/Blog.Smoothies/Views/Blog/ViewModels/BlogViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Modelo.Posts;
+using PagedList;
+
+namespace Blog.Smoothies.Views.Blog.ViewModels
+{
+    public class BlogViewModel
+    {
+
+        public IPagedList<LineaResumenPost> Posts { get; set; }
+
+    }
+}
